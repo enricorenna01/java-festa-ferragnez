1e7cdba63c61d46e8ecac2d9a8d03ad3296e5d6a
enricorenna01 <erenna39@gmail.com>
2025-01-24 18:32:46 +0100

    project init

---
+++ src/com/ferragnez/party/CheckGuest.java
@@ -0,0 +1,12 @@
+package com.ferragnez.party;
+
+import java.util.Scanner;
+
+public class CheckGuest {
+    public static void main(String[] args) {
+        
+        Scanner input = newScanner(System.In);
+
+        input.close();
+    }
+}
