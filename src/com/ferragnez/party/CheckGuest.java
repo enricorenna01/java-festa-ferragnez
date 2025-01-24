5fc0f0287387e2ab8a9c4ad8ecb03d8d5e8462b3
enricorenna01 <erenna39@gmail.com>
2025-01-24 18:36:32 +0100

    feat: added guest list

--- src/com/ferragnez/party/CheckGuest.java
+++ src/com/ferragnez/party/CheckGuest.java
@@ -4,7 +4,8 @@ import java.util.Scanner;
 
 public class CheckGuest {
     public static void main(String[] args) {
-        
+        String[] guestList = { "Dua Lipa", "Paris Hilton", "Manuel Agnelli", "J-Ax", "Francesco Totti", "Ilary Blasi",
+                "Bebe Vio", "Luis", "Pardis Zarei", "Martina Maccherone", "Rachel Zeilic" };
         Scanner input = newScanner(System.In);
 
         input.close();
