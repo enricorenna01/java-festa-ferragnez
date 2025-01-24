cab6c10f75d11804528a4a157bd38d5bda848a4b
enricorenna01 <erenna39@gmail.com>
2025-01-24 19:09:33 +0100

    feat: added list check

--- src/com/ferragnez/party/CheckGuest.java
+++ src/com/ferragnez/party/CheckGuest.java
@@ -4,10 +4,21 @@ import java.util.Scanner;
 
 public class CheckGuest {
     public static void main(String[] args) {
-        String[] guestList = { "Dua Lipa", "Paris Hilton", "Manuel Agnelli", "J-Ax", "Francesco Totti", "Ilary Blasi",
-                "Bebe Vio", "Luis", "Pardis Zarei", "Martina Maccherone", "Rachel Zeilic" };
-        Scanner input = newScanner(System.In);
-
+        String[] guestList = {"Dua Lipa", "Paris Hilton", "Manuel Agnelli", "J-Ax", "Francesco Totti", "Ilary Blasi",
+                "Bebe Vio", "Luis", "Pardis Zarei", "Martina Maccherone", "Rachel Zeilic"};
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("Come ti chiami?");
+        String userName = input.nextLine();
         input.close();
+
+        for(int i = 0; i < guestList.length; i++ ) {
+            if (userName.equals(guestList[i])) {
+                System.out.println("Sei nella lista, puoi entrare!");
+                break;
+            } else {
+                System.out.println("Non sei nella lista, non puoi entrare!");
+            }
+        }
     }
 }
